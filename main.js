6d98f2e09aef5e4f6724cdc6d3d9d9a56b775d89
SharpAceX <sharpacex@gmail.com>
2017-02-24 20:06:05 -0500

    Electron file deleted

--- main.js
+++
@@ -1,7 +0,0 @@
-const electron = require('electron');
-const {app, BrowserWindow} = electron;
-
-app.on('ready', () => {
-    let win = new BrowserWindow({width:600, height:800});
-    win.loadURL('file://' + __dirname + '/index.html');
-});
