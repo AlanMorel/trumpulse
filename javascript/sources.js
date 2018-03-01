8a68c4836bfe2dcf23242f44a16901e55ba761d3
SharpAceX <sharpacex@gmail.com>
2018-02-28 22:23:42 -0500

    Added new sources!
    Added ABC News, BBC News, Bloomberg, Breitbart News, CBS News, Fox News,
    NBC News, Politico, The Hill and The Wall Street Journal

--- javascript/sources.js
+++ javascript/sources.js
@@ -14,15 +14,40 @@ var sources = {
         desc: "Read Facebook posts from Donald Trump's Facebook account.",
         color: "#3b5998"
     },
+    "abc-news": {
+        id: "abc-news",
+        desc: "Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.",
+        color: "#172126"
+    },
     "associated-press": {
         id: "associated-press",
         desc: "The AP delivers in-depth coverage on the international, politics, lifestyle, business, and entertainment news.",
         color: "#ff322e"
     },
+    "bbc-news": {
+        id: "bbc-news",
+        desc: "Use BBC News for up-to-the-minute news, breaking news, video, audio and feature stories. BBC News provides trusted World and UK news as well as local and regional perspectives. Also entertainment, business, science, technology and health news.",
+        color: "#9d0a0e"
+    },
+    "bloomberg": {
+        id: "bloomberg",
+        desc: "Bloomberg delivers business and markets news, data, analysis, and video to the world, featuring stories from Businessweek and Bloomberg News.",
+        color: "#2b00f7"
+    },
+    "breitbart-news": {
+        id: "breitbart-news",
+        desc: "Syndicated news and opinion website providing continuously updated headlines to top news and analysis sources.",
+        color: "#ff5400"
+    },
     "business-insider": {
         id: "business-insider",
         desc: "Business Insider is a fast-growing business site with deep financial, media, tech, and other industry verticals. Launched in 2007, the site is now the largest business news site on the web.",
         color: "#1b607c"
+    },
+    "cbs-news": {
+        id: "cbs-news",
+        desc: "CBS News: dedicated to providing the best in journalism under standards it pioneered at the dawn of radio and television and continue in the digital age.",
+        color: "#222222"
     },
     "cnbc": {
         id: "cnbc",
@@ -34,6 +59,11 @@ var sources = {
         desc: "View the latest news and breaking news today for U.S., world, weather, entertainment, politics and health at CNN.",
         color: "#cc0000"
     },
+    "fox-news": {
+        id: "fox-news",
+        desc: "Breaking News, Latest News and Current News from FOXNews.com. Breaking news and video. Latest Current News: U.S., World, Entertainment, Health, Business, Technology, Politics, Sports.",
+        color: "#003066"
+    },
     "google-news": {
         id: "google-news",
         desc: "Comprehensive, up-to-date news coverage, aggregated from sources all over the world by Google News.",
@@ -43,6 +73,11 @@ var sources = {
         id: "independent",
         desc: "National morning quality (tabloid) includes free online access to news and supplements. Insight by Robert Fisk and various other columnists.",
         color: "#ed1b2f"
+    },
+    "nbc-news": {
+        id: "nbc-news",
+        desc: "Breaking news, videos, and the latest top stories in world news, business, politics, health and pop culture.",
+        color: "#103850"
     },
     "newsweek": {
         id: "newsweek",
@@ -54,10 +89,20 @@ var sources = {
         desc: "NYMAG and New York magazine cover the new, the undiscovered, the next in politics, culture, food, fashion, and behavior nationally, through a New York lens.",
         color: "#000000"
     },
+    "politico": {
+        id: "politico",
+        desc: "Political news about Congress, the White House, campaigns, lobbyists and issues.",
+        color: "#b70000"
+    },
     "reuters": {
         id: "reuters",
         desc: "Reuters.com brings you the latest news from around the world, covering breaking news in business, politics, entertainment, technology, video and pictures.",
         color: "#f47a1f"
+    },
+    "the-hill": {
+        id: "the-hill",
+        desc: "The Hill is a top US political website, read by the White House and more lawmakers than any other site -- vital for policy, politics and election campaigns.",
+        color: "#2a52c1"
     },
     "the-huffington-post": {
         id: "the-huffington-post",
@@ -68,6 +113,11 @@ var sources = {
         id: "the-new-york-times",
         desc: "The New York Times: Find breaking news, multimedia, reviews & opinion on Washington, business, sports, movies, travel, books, jobs, education, real estate, cars & more at nytimes.com.",
         color: "#000000"
+    },
+    "the-wall-street-journal": {
+        id: "the-wall-street-journal",
+        desc: "WSJ online coverage of breaking news and current headlines from the US and around the world. Top stories, photos, videos, detailed analysis and in-depth reporting.",
+        color: "#111"
     },
     "the-washington-post": {
         id: "the-washington-post",
